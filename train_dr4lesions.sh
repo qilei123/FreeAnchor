ad8714d4c97ce137407746a0c68bbf4e09f7ce6c
qilei123 <qileimail123@gmail.com>
2019-09-20 15:16:47 -0400

    test

--- train_dr4lesions.sh
+++ train_dr4lesions.sh
@@ -1,1 +1,1 @@
-python3 tools/train_net.py --config-file configs/free_anchor_R-101-FPN_1x_DR_4lesions.yaml --skip-test false opts
+python3 tools/train_net.py --config-file configs/free_anchor_R-101-FPN_1x_DR_4lesions.yaml --skip-test 0 opts
