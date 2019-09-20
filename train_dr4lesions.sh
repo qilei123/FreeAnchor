a4caf56192b7a81cc0bacea9b4515fc5ad6cfe5b
qilei123 <qileimail123@gmail.com>
2019-09-20 15:30:30 -0400

    test

--- train_dr4lesions.sh
+++ train_dr4lesions.sh
@@ -1,3 +1,3 @@
 #python3 tools/train_net.py --config-file configs/free_anchor_R-101-FPN_1x_DR_4lesions.yaml --skip-test 0 opts
 export NGPUS=2
-python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/free_anchor_R-101-FPN_1x_DR_4lesions.yaml" --skip-test false 
+python3 -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/free_anchor_R-101-FPN_1x_DR_4lesions.yaml" --skip-test false 
