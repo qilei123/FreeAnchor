80361a7addb7d84a50863a6b34734d28034c7256
qilei123 <qileimail123@gmail.com>
2019-09-23 12:37:33 -0400

    test

---
+++ train_rop_ridge.sh
@@ -0,0 +1,4 @@
+#python3 tools/train_net.py --config-file configs/free_anchor_R-101-FPN_1x_DR_4lesions.yaml --skip-test 0 opts
+export NGPUS=2
+python3 -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py --config-file "configs/free_anchor_R-101-FPN_1x_ROP_ridge.yaml"
+# --skip-test "false" 
